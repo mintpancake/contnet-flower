d03e8c8dd37f1f5e692073a001d2674b20c42965
Dennis <bouncy.lu@hotmail.com>
2022-10-06 16:55:47 +0800

    Revert "Update resnet.py"

--- models/resnet.py
+++ models/resnet.py
@@ -7,40 +7,6 @@
 
 import torch
 import torch.nn as nn
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import matplotlib.pyplot as plt
-
-# data augmentation
-datagen = ImageDataGenerator(
-      rotation_range=40,
-      width_shift_range=0.2,
-      height_shift_range=0.2,
-      shear_range=0.2,
-      zoom_range=0.2,
-      horizontal_flip=True,
-      fill_mode='nearest')
-
-# This is module with image preprocessing utilities
-from tensorflow.keras.preprocessing import image
-fnames = [os.path.join(train_dir, fname) for fname in os.listdir(train_dir)]
-# We pick one image to "augment"
-img_path = fnames[3]
-# Read the image and resize it
-img = image.load_img(img_path, target_size=(150, 150))
-# Convert it to a Numpy array with shape (150, 150, 3)
-x = image.img_to_array(img)
-# Reshape it to (1, 150, 150, 3)
-x = x.reshape((1,) + x.shape)
-# The .flow() command below generates batches of randomly transformed images.
-# It will loop indefinitely, so we need to `break` the loop at some point!
-i = 0
-for batch in datagen.flow(x, batch_size=1):
-    plt.figure(i)
-    imgplot = plt.imshow(image.array_to_img(batch[0]))
-    i += 1
-    if i % 4 == 0:
-        break
-plt.show()
 
 
 class ResBlock(nn.Module):
