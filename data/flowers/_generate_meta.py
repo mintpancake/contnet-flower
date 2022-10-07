044abccd39ce8b6a3457f33927f2657a661707e9
mintpancake <bouncy.lu@hotmail.com>
2022-10-07 13:42:33 +0800

    Update split gen

--- data/flowers/_generate_meta.py
+++ data/flowers/_generate_meta.py
@@ -7,10 +7,11 @@ classes = os.listdir('train')
 classes = sorted(classes)
 for c in classes:
     print(c)
-    class_id = c.split("_")[-1]
+    class_id = int(c.split("_")[-1])
     cur_files = os.listdir(os.path.join('train', c))
+    cur_files = sorted(cur_files)
     for cur_file in cur_files:
-        line = root_dir + 'train/' + c + '/' + cur_file + ' ' + class_id + '\n'
+        line = root_dir + 'train/' + c + '/' + cur_file + ' ' + str(class_id) + '\n'
         f.write(line)
 f.close()
 
@@ -19,10 +20,11 @@ classes = os.listdir('val')
 classes = sorted(classes)
 for c in classes:
     print(c)
-    class_id = c.split("_")[-1]
+    class_id = int(c.split("_")[-1])
     cur_files = os.listdir(os.path.join('val', c))
+    cur_files = sorted(cur_files)
     for cur_file in cur_files:
-        line = root_dir + 'val/' + c + '/' + cur_file + ' ' + class_id + '\n'
+        line = root_dir + 'val/' + c + '/' + cur_file + ' ' + str(class_id) + '\n'
         f.write(line)
 f.close()
 
@@ -31,9 +33,10 @@ classes = os.listdir('test')
 classes = sorted(classes)
 for c in classes:
     print(c)
-    class_id = c.split("_")[-1]
+    class_id = int(c.split("_")[-1])
     cur_files = os.listdir(os.path.join('test', c))
+    cur_files = sorted(cur_files)
     for cur_file in cur_files:
-        line = root_dir + 'test/' + c + '/' + cur_file + ' ' + class_id + '\n'
+        line = root_dir + 'test/' + c + '/' + cur_file + ' ' + str(class_id) + '\n'
         f.write(line)
 f.close()
