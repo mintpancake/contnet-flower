83a24479d3439a8d3a1def4b03070288f5e07b89
AQing-527 <54972646+AQing-527@users.noreply.github.com>
2022-10-10 19:35:50 +0800

    transformation and normalization (#11)
    * transformation and normalization
    
    * normalize val data

--- train.py
+++ train.py
@@ -4,28 +4,31 @@ import argparse
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader, ConcatDataset
-from torchvision import transforms
+from torchvision import transforms, io
 from models.resnet import ResNet, ResBlock, ResBottleneckBlock
 from dataset import FlowersDataset
 import utils
+from PIL import Image
 
 from torch.utils.tensorboard import SummaryWriter
-
-# TODO: Data normalization (may require offline data augmentation); Save log for visualization
-
 
 class Trainer():
     def __init__(self, config_path, data_path, save_path, log_path):
         self.config = utils.load_config(config_path)
         self.save_path = os.path.join(save_path, f'{utils.current_time()}')
-        self.loss_log_path = os.path.join(log_path, 'loss/', f'{utils.current_time()}')
-        self.acc_log_path = os.path.join(log_path, 'acc/', f'{utils.current_time()}')
-        self.train_meta_path = os.path.join(data_path, 'meta/train.txt')
+        self.loss_log_path = os.path.join(
+            log_path, 'loss/', f'{utils.current_time()}')
+        self.acc_log_path = os.path.join(
+            log_path, 'acc/', f'{utils.current_time()}')
+        self.train_meta_path = os.path.join(
+            data_path, 'transformed/meta/train.txt')
         self.eval_meta_path = os.path.join(data_path, 'meta/val.txt')
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.train_data = self.get_dataset(self.train_meta_path)
+        self.train_data = FlowersDataset(meta_file=self.train_meta_path, transform=transforms.Compose(
+            [transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize(mean=[0.4493, 0.4431, 0.2898], std=[0.2810, 0.2535, 0.2626])]))
         self.val_data = FlowersDataset(meta_file=self.eval_meta_path, transform=transforms.Compose(
-            [transforms.ToPILImage(), transforms.Resize([224, 224]), transforms.ToTensor()]))
+            [transforms.ToPILImage(), transforms.Resize([224, 224]), transforms.ToTensor(),
+             transforms.Normalize(mean=[0.4493, 0.4431, 0.2898], std=[0.2810, 0.2535, 0.2626])]))
         self.train_loader = DataLoader(
             self.train_data, batch_size=self.config["batch_size"], shuffle=self.config["shuffle"], drop_last=self.config["drop_last"])
         self.val_loader = DataLoader(
@@ -39,56 +42,10 @@ class Trainer():
         self.total_val_step = 0
         self.start_time = 0.0
         self.end_time = 0.0
-
-    def get_dataset(self, meta_path):
-        # https://pytorch.org/vision/stable/transforms.html
-        torch.manual_seed(7)
-        transform1 = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize([256, 256]),
-            transforms.RandomCrop([224, 224]),
-            transforms.ColorJitter(brightness=0.5),
-            transforms.RandomRotation(degrees=45),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.05),
-            transforms.ToTensor(),
-            # Normalization
-            transforms.Normalize(mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0])
-        ])
-        transformed_dataset1 = FlowersDataset(
-            meta_file=meta_path, transform=transform1)
-        torch.manual_seed(17)
-        transform2 = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize([256, 256]),
-            transforms.RandomCrop([224, 224]),
-            transforms.ColorJitter(brightness=0.5),
-            transforms.RandomRotation(degrees=45),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.05),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0])
-        ])
-        transformed_dataset2 = FlowersDataset(
-            meta_file=meta_path, transform=transform2)
-        torch.manual_seed(27)
-        transform3 = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize([256, 256]),
-            transforms.RandomCrop([224, 224]),
-            transforms.ColorJitter(brightness=0.5),
-            transforms.RandomRotation(degrees=45),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.05),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0])
-        ])
-        transformed_dataset3 = FlowersDataset(
-            meta_file=meta_path, transform=transform3)
-        return ConcatDataset([transformed_dataset1, transformed_dataset2, transformed_dataset3])
+        self.loss_writer = SummaryWriter(self.loss_log_path)
+        self.acc_writer = SummaryWriter(self.acc_log_path)
 
     def train(self):
-        writer = SummaryWriter(self.loss_log_path)
         self.model.train()
         size = len(self.train_loader.dataset)
         for batch, (img, label) in enumerate(self.train_loader):
@@ -104,12 +61,11 @@ class Trainer():
             self.total_train_step += 1
             # save the loss info of steps to loss log file
             if self.total_train_step % self.config["save_loss_log_steps"] == 0:
-                writer.add_scalar('Training loss', loss.item(), self.total_train_step)
+                self.loss_writer.add_scalar(
+                    'Training loss', loss.item(), self.total_train_step)
                 pass
-        writer.close()
 
     def eval(self):
-        writer = SummaryWriter(self.acc_log_path)
         self.model.eval()
         size = len(self.val_loader.dataset)
         loss_fn = nn.CrossEntropyLoss(reduction='sum').to(self.device)
@@ -134,8 +90,8 @@ class Trainer():
         self.total_val_step += 1
         # save the accuracy info of epochs to acc log file
         if self.total_val_step % self.config["save_acc_log_steps"] == 0:
-            writer.add_scalar('Training accuracy', val_acc, self.total_val_step)
-        writer.close()
+            self.acc_writer.add_scalar(
+                'Training accuracy', val_acc, self.total_val_step)
 
     def start(self):
         utils.ensure_dir(self.loss_log_path)
@@ -153,6 +109,8 @@ class Trainer():
                 torch.save(self.model.state_dict(), pth_path)
         pth_path = os.path.join(self.save_path, 'latest.pth')
         torch.save(self.model.state_dict(), pth_path)
+        self.loss_writer.close()
+        self.acc_writer.close()
         print(
             f'Completed {self.config["epochs"]} epoches; saved in "{self.save_path}"')
 
